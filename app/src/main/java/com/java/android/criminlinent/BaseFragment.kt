c5f353063ce07c82b2b0b376460c5288a07f7f52
Michael Vlascitchii <mishavlashitschii@gmail.com>
2023-09-23 15:52:40 +0300

    CI-03 Add BaseFragment class

---
+++ app/src/main/java/com/java/android/criminlinent/BaseFragment.kt
@@ -0,0 +1,31 @@
+package com.java.android.criminlinent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB : ViewBinding>(
+    private val bindingInflater: (inflater: LayoutInflater, container: ViewGroup?, bool: Boolean) -> VB
+) : Fragment() {
+
+    private var _binding: VB? = null
+    protected val binding: VB
+        get() = _binding as VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = bindingInflater.invoke(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
